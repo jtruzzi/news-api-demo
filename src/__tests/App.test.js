0c3ac15588e59930351f2ec31a00cbf6473e302b
Julio Truzzi <julio.truzzi@gmail.com>
2019-06-17 21:22:07 -0300

    Remove unused packages. Rollback tests

--- src/__tests/App.test.js
+++ src/__tests/App.test.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { configure, shallow } from "enzyme";
-import { expect } from "chai";
+import ReactDOM from "react-dom";
 import App from "../App";
-import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() });
-
-describe("App component testing", function() {
-  it("renders welcome message", function() {
-    const wrapper = shallow(<App />);
-    const healthMenu = <div class="css-d12kpy col-12">NO RESULTS</div>;
-    console.info(wrapper);
-    expect(wrapper.contains(healthMenu)).to.equal(true);
-  });
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
